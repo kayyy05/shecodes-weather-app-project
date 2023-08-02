7eb124a3b5c99f1d3fad5c5e31c4faeeeb46c7b1
kayyy05 <kalenga.lumbiwe@gmail.com>
2023-08-02 21:53:06 +0200

    Cleaned up js and fixed bugs

--- src/app.js
+++ src/app.js
@@ -22,30 +22,35 @@ let currentTime = `${day} ${time}`;
 let currentDate = document.querySelector("#day-time");
 currentDate.innerHTML = currentTime;
 
-function search(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-city").value;
-  let title = document.querySelector("h1");
-  title.innerHTML = input;
+function displayTemperature(response) {
+  let name = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let descriptionElement = response.data.weather[0].description;
+  let humidity = response.data.main.humidity;
+  let wind = Math.round(response.data.wind.speed * 3.6);
+  let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 
+  celsiusTemp = response.data.main.temp;
+
+  icon.setAttribute("src", iconUrl);
+  icon.setAttribute("alt", response.data.weather[0].description);
+  document.querySelector("h1").innerHTML = name;
+  document.querySelector("#temp-digits").innerHTML = temperature;
+  document.querySelector("#condition").innerHTML = descriptionElement;
+  document.querySelector("#humidity").innerHTML = `${humidity}%`;
+  document.querySelector("#wind").innerHTML = `${wind}km/h`;
+}
+
+function search(input) {
   let apiKey = "1e473372851a26d6162e6984afe7be2f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(function (response) {
-    let temperature = Math.round(response.data.main.temp);
-    let descriptionElement = response.data.weather[0].description;
-    let humidity = response.data.main.humidity;
-    let wind = Math.round(response.data.wind.speed * 3.6);
-    let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-    celsiusTemp = response.data.main.temp;
-
-    icon.setAttribute("src", iconUrl);
-    icon.setAttribute("alt", response.data.weather[0].description);
-    document.querySelector("#temp-digits").innerHTML = temperature;
-    document.querySelector("#condition").innerHTML = descriptionElement;
-    document.querySelector("#humidity").innerHTML = `${humidity}%`;
-    document.querySelector("#wind").innerHTML = `${wind}km/h`;
-  });
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-city");
+  search(cityInput.value);
 }
 
 function displayFahrenheitTemp(event) {
@@ -70,8 +75,13 @@ let celsiusTemp = null;
 let citySearch = document.querySelector("form");
 citySearch.addEventListener("submit", search);
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
 let fahrenheitLink = document.querySelector("#fahrenheit-unit");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-unit");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+search("Lusaka");
